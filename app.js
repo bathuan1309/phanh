7b69ce7cab5a5eb4e3b17816ad111c58132efd89
Admin <bathuan1309@gmail.com>
2021-08-15 08:41:18 +0700

    asd

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
+	swal("aaa", "a", "as");
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
